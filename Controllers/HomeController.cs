5b3057fa065a63cc43d14b05fea0b3a736b2638b
robot <robot>
2016-12-12 21:57:05 -0600

    Rows

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace Project1.Controllers
 {
     public class HomeController : Controller
@@ -31,5 +32,6 @@ namespace Project1.Controllers
         {
             return View();
         }
+
     }
 }
